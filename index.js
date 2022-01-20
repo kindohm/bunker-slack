82b51afa6cb270ea5240232bcd5c7e28afd9d975
Mike Hodnick <mike@kindohm.com>
2022-01-20 11:57:20 -0600

    2nd

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/magic8ball', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/magic8ball', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
