efc65565da98c99a8720b8d63599a81535d8537c
Mike Hodnick <mike@kindohm.com>
2022-01-20 13:24:27 -0600

    make better

--- index.js
+++ index.js
@@ -1,78 +1,51 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5150;
 const bodyParser = require("body-parser");
+const answers = require('./answers')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// sourced from https://en.wikipedia.org/wiki/Magic_8-ball
-const answers = [
-  "It is certain.",
-  "It is decidedly so.",
-  "Without a doubt.",
-  "Yes definitely.",
-  "You may rely on it.",
-
-  "As I see it, yes.",
-  "Most likely.",
-  "Outlook good.",
-  "Yes.",
-  "Signs point to yes.",
-
-  "Reply hazy, try again.",
-  "Ask again later.",
-  "Better not tell you now.",
-  "Cannot predict now.",
-  "Concentrate and ask again.",
-
-  "Don't count on it.",
-  "My reply is no.",
-  "My sources say no.",
-  "Outlook not so good.",
-  "Very doubtful. ",
-];
-
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-}
+const randInt = (min, max) => {
+  const cmin = Math.ceil(min);
+  const fmax = Math.floor(max);
+  return Math.floor(Math.random() * (fmax - cmin + 1) + cmin);
+};
 
 app.post("/magic8ball", (req, res) => {
-  console.log("req.body", req.body);
+  try {
+    const answer = answers[randInt(0, answers.length - 1)];
+    const { body } = req;
+    const { text, user_name } = body;
 
-  const answer = answers[getRandomIntInclusive(0, answers.length - 1)];
-  const { body } = req;
-  const { text, user_name } = body;
-
-  const slackResponse = {
-    response_type: "in_channel",
-    blocks: [
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: `_${user_name} shakes the magic 8 ball and asks \"${text}\"_`,
+    const slackResponse = {
+      response_type: "in_channel",
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `_${user_name} shakes the magic 8 ball and asks \"${text}\"_`,
+          },
         },
-      },
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: `${answer}`,
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `${answer}`,
+          },
         },
-      },
-    ],
-  };
+      ],
+    };
 
-  res.json(slackResponse);
-
-  // res.json({
-  //   response_type: "in_channel",
-  //   text,
-  // });
+    res.json(slackResponse);
+  } catch (e) {
+    console.error("an error occurred");
+    console.error(e);
+    res.status(500).send("error");
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening on port ${port}`);
 });
