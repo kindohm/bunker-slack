4f6bb2bc4cdaa02218ba0b38aa97677785f3d3b6
Mike Hodnick <mike@kindohm.com>
2022-01-20 12:14:15 -0600

    return json

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/magic8ball', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.post('/magic8ball', (req, res) => {
-  res.send('Hello World!')
-})
+app.post("/magic8ball", (req, res) => {
+  res.json({
+    response_type: "in_channel",
+    text: "It's 80 degrees right now.",
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
