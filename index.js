564f779355b5a2a288f6b6e0d1c5978dad658076
Mike Hodnick <mike@kindohm.com>
2022-01-20 12:33:19 -0600

    add random responses

--- index.js
+++ index.js
@@ -2,11 +2,45 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// sourced from https://en.wikipedia.org/wiki/Magic_8-ball
+const answers = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes definitely.",
+  "You may rely on it.",
+
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful. ",
+];
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
 
 app.post("/magic8ball", (req, res) => {
+  const text = answers[getRandomIntInclusive(0,answers.length-1)]
   res.json({
     response_type: "in_channel",
-    text: "It's 80 degrees right now.",
+    text,
   });
 });
 
