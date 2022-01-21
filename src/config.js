d914d78e2f6a6647534a2a46d33dfd5befda57b5
Mike Hodnick <mike@kindohm.com>
2022-01-21 09:25:47 -0600

    implement tracery soulsphere

--- src/config.js
+++
@@ -1,7 +0,0 @@
-const WORDNIK_API_URL = process.env.WORDNIK_API_URL;
-const WORDNIK_API_KEY = process.env.WORDNIK_API_KEY;
-
-module.exports = {
-  wordnikApiUrl: WORDNIK_API_URL,
-  wordnikApiKey: WORDNIK_API_KEY,
-};
--- src/soulSphereHandler.js
+++ src/soulSphereHandler.js
@@ -1,28 +1,13 @@
-const axios = require('axios');
-const { wordnikApiUrl, wordnikApiKey } = require('./config');
+const tracery = require('tracery-grammar');
 const { randInt } = require('./util');
-const words = require('./words');
-
-const getRandomWord = async () => {
-  return words[randInt(0, words.length - 1)];
-  // const url = `${wordnikApiUrl}/words.json/randomWord?api_key=${wordnikApiKey}`;
-  // const result = await axios.get(url);
-  // return result.data.word;
-};
+const structures = require('./structures');
 
 const getRandomSentence = async () => {
-  try {
-    const word = await getRandomWord();
-    const url = `${wordnikApiUrl}/word.json/${word}/examples?limit=50&api_key=${wordnikApiKey}`;
-    const result = await axios.get(url);
-    const examples = result.data.examples;
-    console.log('word', word);
-    return examples[randInt(0, examples.length - 1)].text;
-  } catch (err) {
-    console.error('error getting sentence. probably exceeded rate limit.');
-    console.error(err);
-    return 'Bunker is currently soulless.';
-  }
+  const grammar = tracery.createGrammar(
+    structures[randInt(0, structures.length - 1)]
+  );
+  grammar.addModifiers(tracery.baseEngModifiers);
+  return grammar.flatten('#origin#');
 };
 
 const handler = async (req, res) => {
