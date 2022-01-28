1e02f037cdcec2361b78923b92b9e72ac80de3e3
Mike Hodnick <mike@kindohm.com>
2022-01-28 10:36:22 -0600

    remove delay test route

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import magic8ball from './routes/magic8ball';
 import soulSphere from './routes/soulSphere';
-import delay from './routes/delay';
 
 const app = express();
 const port = 5150;
@@ -12,7 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // routes
 app.use('/magic8ball', magic8ball);
 app.use('/soulsphere', soulSphere);
-app.use('/delay', delay);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
