0544e61ecd1bff8563eb388221730b885dfb53cc
Mike Hodnick <mike@kindohm.com>
2022-02-05 11:52:05 -0600

    remove failing test

--- src/routes/hangman/game.spec.ts
+++ src/routes/hangman/game.spec.ts
@@ -1,9 +1,6 @@
 import { createGame, GameState, guess, getGameDisplay } from './game';
 
 describe('game', () => {
-  it('should fail', () => {
-    expect(1).toEqual(2);
-  });
   describe('createGame', () => {
     it('should return new game with initial values', () => {
       const game = createGame('hank', 'hoop');
