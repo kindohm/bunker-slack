0674c7d7f92ac52045cd15fdead5f59565e155d2
Mike Hodnick <mike@kindohm.com>
2024-01-10 14:08:25 -0600

    debug interact payload

--- src/routes/interact/index.ts
+++ src/routes/interact/index.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 const router = express.Router();
 
 router.post('/', (req: Request, res: Response) => {
-  console.log('interact');
+  console.log('interact', JSON.stringify(req.body.payload, null, 2));
   const { body } = req;
 
   console.log('full body', body);
